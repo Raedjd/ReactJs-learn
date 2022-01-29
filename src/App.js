883e15f3778006d220355861b401ed8cfe290447
Your Name <you@example.com>
2022-01-29 16:05:04 +0100

    add a functional component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import {Greet} from './Components/Greet';
+import Hi from './Components/Hi'
 function App() {
   const name= "raed"
   return (
     <div className="App">
       <Greet></Greet>
+      <Hi></Hi>
     </div>
   );
 }
--- src/Components/Greet.js
+++ src/Components/Greet.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+ //functional component
 /* function Greet(){
     return <h1>Hello Read Jaidi!</h1>
 } */
